c8dbc75f923b47b437049ef185012ae48175da74
silenkovkonstantin <silenkov@inbox.ru>
2023-02-26 21:23:33 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Module_9_3_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action showMessageDelegate = ShowMessage;
+            showMessageDelegate.Invoke();
+
+            Func<int, int, int, int> sumDelegate = Sum;
+            int result = sumDelegate.Invoke(1, 30, 120);
+            Console.WriteLine(result);
+
+            Predicate<string> checkLengthDelegate = CheckLength;
+            bool status = checkLengthDelegate.Invoke("skill_factory");
+            Console.WriteLine(status);
+
+            Console.ReadKey();
+        }
+
+        static void ShowMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static bool CheckLength(string _row)
+        {
+            if (_row.Length > 3) return true;
+            return false;
+        }
+    }
+}
